43d29611d558d8ee64daae85135e9db52854f812
Ryan Doenges <rhdoenges@gmail.com>
2013-02-10 00:28:49 -0800

    Added timeout handling + --flags + json output
    - Last command line argument is the scale to select (int >= 0)
    - Re-check first byte of the scale's response every 100ms (by default, can
      be changed with the --retry flag)
    - Timeout on the scale's response after 3000ms (by default, can be changed
      with the --fail flag)
    - output is formatted in json, either:
        {"success":true, "weight":1.15, "units":"lbs"}
      or:
        {"success":false, "error":"Something's gone wrong!"}

--- Example.cs
+++ Example.cs
@@ -15,22 +15,61 @@ namespace ScaleReader
 {
   class Program
   {
+    public static void Error(string message)
+    {
+      Console.Write(@"{{""success"":false,""error"":""{0}""}}", message);
+    }
     public static void Main(string[] args)
     {
+      int scale = 0;
+      int retryTime = 100;
+      int timeoutLength = 3000;
+      for (int i = 0; i < args.Length; i++)
+      {
+        string arg = args[i];
+        if (arg.StartsWith("--"))
+        {
+          // TODO make sure arg matches --[a-z-]+=[0-9]+
+          // Command-line flags take the form `--arg-name=100`.
+          // C#'s Substring() takes a starting index and a /length/.
+          string argName = arg.Substring(2, arg.LastIndexOf('=') - 2);
+          // We're assuming all argument values will be integers.
+          int argValue = Int32.Parse(arg.Substring(arg.LastIndexOf('=') + 1));
+          switch (argName)
+          {
+            case "retry":
+              retryTime = argValue;
+              break;
+            case "fail":
+              timeoutLength = argValue;
+              break;
+          }
+        }
+        // The last argument should always be the scale number.
+        else if (!(Int32.TryParse(arg, out scale) && i == args.Length - 1))
+        {
+          Error(string.Format("Invalid command-line argument. -- {0}", arg));
+        }
+      }
       decimal? weight;
       bool? isStable;
 
-      USBScale s = new USBScale();
+      USBScale s = new USBScale(scale, retryTime, timeoutLength);
       s.Connect();
 
       if (s.IsConnected)
       {
-        s.GetWeight(out weight, out isStable);
+        ScaleWeightStatus status = s.GetWeight(out weight, out isStable);
+        if (status != ScaleWeightStatus.Stable)
+        {
+          Error(USBScale.ErrorStringFor(status));
+        }
         s.DebugScaleData();
         s.Disconnect();
-        Console.WriteLine("Weight: {0:0.00} LBS", weight);
+        // I'm writing out json manually because including a library to do this seems like overkill.
+        Console.Write(@"{{""success"":true,""weight"":{0},""units"":""lbs""}}", weight);
       } else {
-        Console.WriteLine("No Scale Connected.");
+        Error("No Scale Connected.");
       }
     }
   }
